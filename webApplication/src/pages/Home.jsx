6b29186e5822ef33ad0cd37c93bb39da4699a3db
juanbep <jbecapillimue@gmail.com>
2025-05-02 12:26:16 -0500

    efecto particulas

--- webApplication/src/pages/Home.jsx
+++ webApplication/src/pages/Home.jsx
@@ -1,32 +1,36 @@
 import React from "react";
 import { FaUserGraduate, FaBook } from "react-icons/fa";
+import ParticlesEffect from "../components/ParticlesEffect";
 import { useNavigate } from "react-router-dom";
 
 const FastTrackApp = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border rounded-lg p-8 w-full max-w-md">
-      <h1 className="text-2xl font-light text-center mb-8">
-        FASTTRACK APPLICATION
-      </h1>
+    <div>
+      <ParticlesEffect />
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border rounded-lg p-8 w-full max-w-md bg-white bg-opacity-90 z-10">
+        <h1 className="text-2xl font-light text-center mb-8">
+          FASTTRACK APPLICATION
+        </h1>
 
-      <div className="space-y-4">
-        <button
-          onClick={() => navigate("/listarEstudiantes")}
-          className="w-full flex items-center justify-center space-x-3 border py-3 px-4 rounded hover:bg-gray-100 transition cursor-pointer"
-        >
-          <FaUserGraduate className="text-lg" />
-          <span>Gestion Estudiantes</span>
-        </button>
+        <div className="space-y-4">
+          <button
+            onClick={() => navigate("/listarEstudiantes")}
+            className="w-full flex items-center justify-center space-x-3 border py-3 px-4 rounded hover:bg-gray-100 transition cursor-pointer"
+          >
+            <FaUserGraduate className="text-lg" />
+            <span>Gestion Estudiantes</span>
+          </button>
 
-        <button
-          onClick={() => navigate("/listarMaterias")}
-          className="w-full flex items-center justify-center space-x-3 border py-3 px-4 rounded hover:bg-gray-100 transition cursor-pointer"
-        >
-          <FaBook className="text-lg" />
-          <span>Gestion Materias</span>
-        </button>
+          <button
+            onClick={() => navigate("/listarMaterias")}
+            className="w-full flex items-center justify-center space-x-3 border py-3 px-4 rounded hover:bg-gray-100 transition cursor-pointer"
+          >
+            <FaBook className="text-lg" />
+            <span>Gestion Materias</span>
+          </button>
+        </div>
       </div>
     </div>
   );
